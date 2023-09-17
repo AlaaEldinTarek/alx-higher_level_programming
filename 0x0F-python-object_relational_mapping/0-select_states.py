6de004fb8d2072085d03a04288f4ede69210955a
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-09-17 20:20:06 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 Lists all states from the database hbtn_0e_0_usa.
 Usage: ./0-select_states.py <mysql username> \<mysql password> \<database name> 
 """
-from sys import argv
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
