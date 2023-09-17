f7ba72e53db58a8e7e9a3986543d333775ee104c
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-09-17 19:50:21 +0300

    done

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
-import sys
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database
+"""
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
