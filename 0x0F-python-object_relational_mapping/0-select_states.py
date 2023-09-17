efea266015dbe289d4d5289b7f8c1d5b5ef3a6cf
Alaa Eldin Tarek <101481833+AlaaEldinTarek@users.noreply.github.com>
2023-09-17 20:15:06 +0300

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
-from sys import argv
+=== 
+ Lists all states from the database hbtn_0e_0_usa.
+ Usage: ./0-select_states.py <mysql username> \
+                             <mysql password> \
+                             <database name> 
+===
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states")
-     rows = c.fetchall()
-    for i in rows:
-        print(i)
-    db.close()
-    c.close()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
